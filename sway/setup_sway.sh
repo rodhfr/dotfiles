6493168d90714ff20f94aff2b1416ebc085ade8c
Rodolfo Franca <souzafrodolfo@gmail.com>
2024-10-28 16:54:46 -0300

    available to test version

--- sway/setup_sway.sh
+++ sway/setup_sway.sh
@@ -1,74 +1,150 @@
 #!/bin/bash
 
-# WARNINGS
-echo "This script will remove your currently existing lunar vim installation or neovim for what matters"
-echo "Do you which to continue? (y/n): " answer
+# Function to check if a command is installed
+check_installed() {
+    if command -v "$1" > /dev/null; then
+        echo "$1 is installed"
+    else
+        echo "$1 is not installed, there is a problem."
+        exit 1  # Exit the script if the command is not installed
+    fi
+}
 
-if [[ "$answer" == "y"]]; then
+# WARNINGS
+echo "Intended for fresh installs of Debian 12"
+echo "This script will remove your currently existing lunar vim installation or neovim for what matters"
+echo "Make sure to backup your ~/.config/ folder"
+read -p "Do you wish to continue? (y/n): " answer
+
+if [[ "$answer" == "y" ]]; then
   echo "Starting installation..."
+  echo "Installing some basic packages"
   sudo apt update 
   sudo apt upgrade -y
+  if ! sudo apt install -y \
+      git \
+      tar \
+      curl \
+      wget \
+      flatpak \
+      build-essential \
+      python3-pip \
+      wl-clipboard \
+      kitty \
+      clipman \
+      ranger \
+      fuzzel \
+      pipx \
+      grimshot \
+      ripgrep \
+      playerctl \
+      mpv \
+      rclone \
+      pamixer \
+      pavucontrol \
+      pulseaudio-utils \
+      python-is-python3 \
+      yt-dlp; then
+      echo "Failed to install packages." >&2
+      exit 1
 
-  sudo apt install -y \
-    git \
-    flatpak \
-    gnome-software-plugin-flatpak \
-    build-essential \
-    python3-pip \
-    wl-clipboard \
-    kitty \
-    clipman \
-    ranger \
-    fuzzel \
-    pipx \
-    grimshot \
-    ripgrep \
-    playerctl \
-    pamixer \
-    pavucontrol \
-    pulseaudio-utils \
-    clipman \
-  
-  # Set up Python alias
-  echo "alias python=python3" >> ~/.bashrc
-  echo "Alias for python3 as python added to .bashrc"
-  
-  sudo apt remove -y \
-    neovim
+  fi
+  echo "Basic packages successfully installed"
 
+  # Add FlatHub
+  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  
+  # Removing old installation of neovim, debian has old version for lunarvim
+  sudo apt remove -y neovim
+
+  # Backing up old config files
   backup_folder="$HOME/.config/SwaySetup_backup_old_config/"
   mkdir -p "$backup_folder"
-  sudo mv "$HOME/.config/sway/" "$backup_folder"
-  sudo mv "$HOME/.config/lvim/" "$backup_folder"
-  sudo mv "$HOME/.config/neovim/" "$backup_folder" 
+  # Move existing configuration folders to backup
+  [ -d "$HOME/.config/sway/" ] && mv -v "$HOME/.config/sway/" "$backup_folder"
+  [ -d "$HOME/.config/lvim/" ] && mv -v "$HOME/.config/lvim/" "$backup_folder"
+  [ -d "$HOME/.config/neovim/" ] && mv -v "$HOME/.config/neovim/" "$backup_folder"
+  [ -d "$HOME/.config/mpv/" ] && mv -v "$HOME/.config/mpv/" "$backup_folder"
+  [ -d "$HOME/.config/kitty/" ] && mv -v "$HOME/.config/kitty/" "$backup_folder"
+  [ -d "$HOME/.config/ranger/" ] && mv -v "$HOME/.config/ranger/" "$backup_folder"
+  [ -d "$HOME/.config/lazygit/" ] && mv -v "$HOME/.config/lazygit/" "$backup_folder"
   
-  sudo mkdir -p "$HOME/.config/sway/"
-  sudo mkdir -p "$HOME/.config/lvim/"
-  sudo mkdir -p "$HOME/.config/neovim/"
+  # Creating folders "Permission check"
+  mkdir -p "$HOME/.config/sway/"
+  mkdir -p "$HOME/.config/lvim/"
+  mkdir -p "$HOME/.config/neovim/"
+  mkdir -p "$HOME/.config/kitty/"
+  mkdir -p "$HOME/.config/mpv/"
+  mkdir -p "$HOME/.config/ranger/"
+  mkdir -p "$HOME/.config/lazygit/"
+  mkdir -p "$HOME/.config/.BuildSwaySetup"
 
+  # Cloning dotfiles
+  git clone "https://github.com/rodhfr/dotfiles.git" "$HOME/.config/.BuildSwaySetup/"
+  # Moving dotfiles with overwrite
+  cp -rf "$HOME/.config/.BuildSwaySetup/dotfiles/"* "$HOME/.config/"
+  # Removing cached dotfiles
+  rm -r "$HOME/.config/.BuildSwaySetup"
+
+
+  # ---- Install Lunarvim 0.9.5 ----
   
-  
-  # Install Lunar for Neovim 0.9.5 
-  
-  # Setup Rust Latest
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --profile default --no-modify-path
-  echo "export PATH=~/.local/bin:$PATH" >> ~/.bashrc
-  # Dependencies:
-  # Symlink Neovim 0.9.5 binary
-  sudo ln -s ~/.config/neovim/bin/nvim /usr/bin/nvim
-  sudo chmod +x /usr/bin/nvim
-  echo "Symlink for Neovim created"
+  # -- Dependencies --
+  # Package Manager Dependencies
   sudo apt install -y \
     python3-pynvim \
+    python-pip \
     make \
     ripgrep \
-    git \
-  
-  
-  
-  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    git
 
-elif [[ "$answer" == "f"]]; then
+
+  # Symlink Neovim binary
+  if [ -f "$HOME/.config/neovim/bin/nvim" ]; then
+      sudo ln -s "$HOME/.config/neovim/bin/nvim" /usr/bin/
+      sudo chmod +x /usr/bin/nvim
+      echo "Symlink for Neovim created"
+  else
+      echo "Neovim binary not found, skipping symlink creation."
+  fi
+
+  # Symlink Lazygit binary
+  if [ -f "$HOME/.config/lazygit/lazygit" ]; then
+      sudo ln -s "$HOME/.config/lazygit/lazygit" /usr/bin/
+      sudo chmod +x /usr/bin/lazygit
+      echo "Symlink for Lazygit created"
+  else
+      echo "Lazygit binary not found, skipping symlink creation."
+  fi
+
+  # Setup Rust Latest
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --profile default --no-modify-path
+  echo "export PATH=\$HOME/.local/bin:\$PATH" >> "$HOME/.bashrc"
+  source "$HOME/.bashrc"
+  check_installed rustc
+
+  # Install NVM (version manager for node)
+  echo "Installing nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  # Export nvm variables
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  # Install Node.js + npm
+  echo "Installing Node.js + npm via nvm"
+  check_installed nvm
+  nvm install node
+  check_installed node
+
+  # just in case before installing Lunarvim
+  source "$HOME/.bashrc"
+  
+  # -- LunarVim Installation -- 
+  LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
+
+  echo "Installation completed successfully."
+  echo "Your old config files were backed up to $backup_folder"
+
+elif [[ "$answer" == "n" ]]; then
   echo "Exiting..."
 else
-  echo "Invalid input. Please enter 'y' or 'f'. Press 'Ctrl+C' to exit"
+  echo "Invalid input. Please enter 'y' or 'n'. Press 'Ctrl+C' to exit"
