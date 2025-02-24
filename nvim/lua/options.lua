b0480a4c5a709bbc914318360f68b45200a27bfd
rodolfo franca <souzafrodolfo@gmail.com>
2025-02-24 18:36:35 -0300

    nvim modifications

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,3 +6,8 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 vim.opt.relativenumber = true
+-- Set indent options
+vim.opt.tabstop = 4        -- Número de espaços que representa uma tabulação
+vim.opt.shiftwidth = 4     -- Número de espaços para a indentação
+vim.opt.expandtab = true   -- Usa espaços em vez de tabulação
+
