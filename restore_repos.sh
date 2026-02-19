cc9739ffa5462273f25185a13feae82b126aeb6c
rodhfr <souzafrodolfo@gmail.com>
2026-02-19 14:19:26 -0300

    fix(restore_service) don't clone if already exists

--- restore_repos.sh
+++ restore_repos.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-sudo dnf install -y git gh stow
-
-gh auth login
-
-gh repo clone rodhfr/dotfiles "$HOME/dotfiles"
-gh repo clone rodhfr/dotfiles_secret "$HOME/dotfiles_secret"
-
 GREEN="\033[1;32m"
 RED="\033[1;31m"
 RESET="\033[0m"
 
-# Aplica stow
+sudo dnf install -y git gh stow
+gh auth login
+
+clone_if_needed() {
+  local repo=$1
+  local dest=$2
+  if [ -d "$dest" ]; then
+    echo -e "⚠️  $dest já existe, pulando clone..."
+  else
+    gh repo clone "$repo" "$dest"
+  fi
+}
+
 apply_stow() {
   local path=$1
   echo -e "🔗 Aplicando stow em $path..."
@@ -19,6 +24,9 @@ apply_stow() {
   stow .
   echo -e "${GREEN}✅ Stow aplicado em $path${RESET}"
 }
+
+clone_if_needed "rodhfr/dotfiles" "$HOME/dotfiles"
+clone_if_needed "rodhfr/dotfiles_secret" "$HOME/dotfiles_secret"
 
 apply_stow "$HOME/dotfiles"
 apply_stow "$HOME/dotfiles_secret"
