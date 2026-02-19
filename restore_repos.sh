60e0ba9ff5390850936557adb067fa60e02fc36e
rodhfr <souzafrodolfo@gmail.com>
2026-02-19 14:44:13 -0300

    enhance(restore_service): handle crontab

--- restore_repos.sh
+++ restore_repos.sh
@@ -4,7 +4,7 @@ GREEN="\033[1;32m"
 RED="\033[1;31m"
 RESET="\033[0m"
 
-sudo dnf install -y git gh stow
+sudo dnf install -y git gh stow cronie
 gh auth login
 
 clone_if_needed() {
@@ -32,3 +32,11 @@ apply_stow "$HOME/dotfiles"
 apply_stow "$HOME/dotfiles_secret"
 
 echo -e "${GREEN}🎉 Dotfiles instalados com sucesso!${RESET}"
+
+(
+  crontab -l 2>/dev/null
+  echo "0 2 * * * $HOME/dotfiles/update_v4.sh >> /tmp/update_v4.log 2>&1"
+) | crontab -
+
+# exec post install script
+bash "$HOME/dotfiles/post_install.sh"
