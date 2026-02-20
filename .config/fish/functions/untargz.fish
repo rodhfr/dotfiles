16e999691e8a81fe1b314ae0fb6771579da10a59
rodhfr <souzafrodolfo@gmail.com>
2026-02-20 01:49:24 -0300

    auto committed

--- .config/fish/functions/untargz.fish
+++ .config/fish/functions/untargz.fish
@@ -1,3 +1,19 @@
+# function untargz
+#     pv $argv | tar -xz
+# end
+
 function untargz
-    pv $argv | tar -xz
+    if test (count $argv) -eq 0
+        echo "usage: untargz <archive.tar.gz> [destination]"
+        return 1
+    end
+
+    set file $argv[1]
+    set dir $argv[2]
+
+    if test -z "$dir"
+        pv "$file" | tar -xz
+    else
+        pv "$file" | tar -xz -C "$dir"
+    end
 end
